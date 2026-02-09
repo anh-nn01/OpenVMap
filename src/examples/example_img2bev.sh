64eab617178b406d26fa5e81d9920856a6db9d3c
anh-nn01 <anhu@umd.edu>
2026-02-09 13:06:14 -0500

    (incomplete) img2bev script examples

--- src/examples/example_img2bev.sh
+++ src/examples/example_img2bev.sh
@@ -15,13 +15,21 @@ export HOME=/fs/nexus-projects/open_vectormap/ # replace your project directory 
 source $HOME/miniconda3/bin/activate
 export IMG_PATH="$HOME/src/examples/nusc/eg_2/n008-2018-05-21-11-06-59-0400__CAM_FRONT__1526915292912465.jpg"
 
+##################################################################
 # Step 1: Generate 3D point cloud from monocular images using DA3
+##################################################################
 export GLB_PATH="$HOME/src/examples/nusc/eg_2/da3_output/"
 conda activate da3
 cd $HOME/src/processors/
 python3 generate_3Dpc.py --img_path $IMG_PATH --export_dir $GLB_PATH
 
-# Step 2: Generate 2D semantic segmentation from monocular images using SAM3
+##################################################################
+# Step 2: Generate 2D semantic segmentation from monocular images 
+#         using SAM3
+##################################################################
 
 
-# Step 3: Merge 3D point cloud and 2D semantic segmentation to generate BEV voxels
+##################################################################
+# Step 3: Merge 3D point cloud and 2D semantic segmentation 
+#         to generate BEV voxels
+##################################################################
