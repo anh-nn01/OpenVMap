cae58d47857257987d3301c992d8bbcbfd5b90be
anh-nn01 <anhu@umd.edu>
2026-03-03 19:10:03 -0500

    update configs

--- src/configs/cfg_bev.py
+++ src/configs/cfg_bev.py
@@ -13,4 +13,7 @@ SEMANTIC_CLASSES = [
 ###################################
 BEV_HEIGHT = 1.5 # height of interest above the ground level, in meters
 voxel_size = 0.5 # voxel size, in meter
+xlim = (-15,15) # lateral
+ylim = None # dynamically adjusted based on avg ground level / cam height
+zlim = (0, 35)  # depth
 
